a86a079b88f97f4da78a3b7ad635df890b3032b4
Bart <kluczynskib@gmail.com>
2016-09-30 10:01:20 +0100

    creating a new wordpress theme

---
+++ wp-content/themes/innovate/js/app.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){
+
+  $(document).foundation();
+  
+  $( ".nav-toggle" ).click(function() {
+    $(this).toggleClass("open");
+    $("nav").fadeToggle(100);
+
+    return false;
+
+
+  });
+
+});
